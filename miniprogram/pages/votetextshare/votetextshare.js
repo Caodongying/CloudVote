706f94714d0a14a5bc33e7071608d28becd0ce5e
cdy <jsjhcdy@163.com>
2021-05-05 17:24:49 +0800

    完成文字投票-参与投票界面html文件，还需要完善选项范围逻辑

--- miniprogram/pages/votetextshare/votetextshare.js
+++ miniprogram/pages/votetextshare/votetextshare.js
@@ -7,9 +7,13 @@ Page({
    */
   data: {
     //openID:0,
-    voteID:0,
+    voteID:"79550af26090ffeb13f7430b1c7b0d2e",//值应为0，这里是为了方便测试，之后要修改
     userInfo:null,
-    voteStatus:"投票进行中" //投票状态  注意应为""，这里是为了测试
+    voteStatus:"", 
+    voteRecord:"",
+    anonymousStatus:"", //匿名状态
+    multiStatus:"",//多选状态
+    optionVoted:[]//选中的投票项
   },
 
   /**
@@ -26,21 +30,85 @@ Page({
 
     console.log(this.data.userInfo)
     console.log(this.data.userInfo.avatarUrl)
-    this.setVoteStatus()
+    this.getVoteRecord(this.setStatus)
   },
   
-  setVoteStatus(){
-    //根据voteID从数据库获取当前投票信息
-    let voteRecord=wx.cloud.database().collection('voteText').doc(this.data.voteID).get()
+  getVoteRecord(callback){
+    let voteRecord=" "
+    wx.cloud.database().collection('voteText')
+      .doc(this.data.voteID)
+      .get()
+      .then(res=>{
+        console.log("查询结果为：",res.data.voteRecord)
+        voteRecord=res.data.voteRecord
+        this.setData({
+          voteRecord:voteRecord
+        })
+        if(typeof callback != "undefined")
+          callback();// 执行调用函数
+      })
+      .catch(console.error)
+  },
+
+  setStatus(){
     //判断投票是否已开始
     let currentTime=app.getCurrentTime()
-    if(app.getCurrentDate()==voteRecord.dateBegin){ //日期已到
+    let currentDate=app.getCurrentDate()
+    let voteRecord=this.data.voteRecord
+    if(currentDate==voteRecord.dateBegin){ //日期已到
       if((currentTime>=voteRecord.timeBegin)&&(currentTime<=voteRecord.timeEnd)){
-        this.data.voteStatus="投票进行中"
+        this.setData({
+          voteStatus:"投票进行中"
+        })
+      }
+      else if(currentTime>=voteRecord.timeEnd){ //同一天，时间已过
+        this.setData({
+          voteStatus:"投票已结束"
+        })
       }
     }
-    else{
-      this.data.voteStatus="投票还未开始"
+    else if(currentDate>=voteRecord.dateEnd){ //日期已过
+      this.setData({
+        voteStatus:"投票已结束"
+      })
     }
+    else{
+      this.setData({
+        voteStatus:"投票还未开始"
+      })
+    }
+
+    //判断是否匿名
+    if(voteRecord.isAnonymous){
+      this.setData({
+        anonymousStatus:"匿名"
+      })
+    }
+    else{
+      this.setData({
+        anonymousStatus:"实名"
+      })
+    }
+
+    //判断是否多选
+    if(voteRecord.isMultiVote){
+      this.setData({
+        multiStatus:"多选，可投"+voteRecord.minVoteNum+"到"+voteRecord.maxVoteNum+"票"
+      })
+    }
+    else{
+      this.setData({
+        multiStatus:"单选"
+      })
+    }
+  },
+
+  chooseOption(e){
+    this.setData({
+      optionVoted:e.detail
+    })
   }
+
+
+ 
 })
